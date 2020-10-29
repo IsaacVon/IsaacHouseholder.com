60716aa37719f2cd373e2f23af10e4d11d747e91
Isaac Householder <isaachouseholder@gmail.com>
2020-10-29 16:50:44 -0700

    populating content for profile, header, objective

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import profile from "../assets/profile.png";
 
 export default function Header() {
   return (
     <>
-    
+
+        <img src={profile} alt="profile" className="profile"/>
+
       <p>Isaac Householder</p>
       <p>Bachelor of Science - Mechanical Engineering</p>
       <p>Irvine, California</p>
--- src/components/objective.jsx
+++ src/components/objective.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 
-
 export default function Objective() {
-
   return (
-    <p>
-      Objective
-    </p>
+    <>
+      <p>Objective</p>
+      <p>
+        The objective of my next career move is to get into the sales side of
+        engineering. I love working with people, and many people have told me
+        that I would be great at sales. After gaining technical industry
+        experience, I am now going to steer my career in this direction because
+        I am confident that things will go great and I will continue to love the
+        work I do. I plan on moving to Kauai, Hawaii this year with my fiancé
+        and I am searching for a remote engineering job that will allow me to
+        pursue my passion in engineering as well as live where I want.
+      </p>
+    </>
   );
 }
