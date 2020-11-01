d52b1841abe1cbfd732f180d82a5c98c8d3d57e3
Isaac Householder <isaachouseholder@gmail.com>
2020-11-01 01:05:54 -0700

    implementing scroll snap

--- src/components/objective.jsx
+++ src/components/objective.jsx
@@ -1,24 +1,24 @@
 import React from "react";
 
-
-
 export default function Objective() {
   return (
     <>
-      <div id="objective" className="spacer"></div>
-
-      <h4  className="color">Objective</h4>
-      <p className="objectiveText">
-        The objective of my next career move is to get into the sales side of
-        engineering. I love working with people, and many people have told me
-        that I would be great at sales. After gaining technical industry
-        experience, I am now going to steer my career in this direction because
-        I am confident that things will go great and I will continue to love the
-        work I do. I plan on moving to Kauai, Hawaii this year with my fiancé
-        and I am searching for a remote engineering job that will allow me to
-        pursue my passion in engineering as well as live where I want.
-      </p>
-      
+      <div className="scrollSnap">
+        <div>
+          <h4 className="color">Objective</h4>
+          <p className="objectiveText">
+            The objective of my next career move is to get into the sales side
+            of engineering. I love working with people, and many people have
+            told me that I would be great at sales. After gaining technical
+            industry experience, I am now going to steer my career in this
+            direction because I am confident that things will go great and I
+            will continue to love the work I do. I plan on moving to Kauai,
+            Hawaii this year with my fiancé and I am searching for a remote
+            engineering job that will allow me to pursue my passion in
+            engineering as well as live where I want.
+          </p>
+        </div>
+      </div>
     </>
   );
 }
