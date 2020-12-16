48b1130918f675fe80e8c665e27f4038f4768378
Isaac Householder <isaachouseholder@gmail.com>
2020-12-15 18:02:42 -0800

    navbar mobile added

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 
+import swirlBackground from "../assets/swirlBackground.jpg";
 import profile from "../assets/profile.png";
-import textBackground from "../assets/swirlBackground2.png";
+import burgerSolid from "../assets/buttons/burgerSolid.png";
+import burgerHover from "../assets/buttons/burgerHover.png";
+import textBackground from "../assets/swirlBackground3.png";
 import line from "../assets/lines/line.png";
 import line2 from "../assets/lines/line2.png";
 
@@ -12,6 +15,12 @@ const Wrapper = styled.section`
   justify-content: center;
   align-items: center;
   margin: 20px 20px;
+`;
+
+const NavDrawer = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `;
 
 const LeftContainer = styled.section`
@@ -26,6 +35,13 @@ const CenterContainer = styled.section`
   /* background-color: darkgreen; */
 `;
 
+const BurgerContainer = styled.section`
+  /* background-color: darkgreen; */
+  position: absolute;
+  right: 29px;
+  top: 43px;
+`;
+
 const RightContainer = styled.section`
   /* background-color: lightcoral; */
   display: flex;
@@ -36,6 +52,10 @@ const RightContainer = styled.section`
 
 const ProfileImage = styled.img`
   width: 104px;
+`;
+
+const Burger = styled.img`
+  width: 55px;
 `;
 
 const LineTop = styled.img`
@@ -58,6 +78,17 @@ const LineBottom = styled.img`
   left: 10px;
 `;
 
+const LineBottomNav = styled.img`
+  margin-top: 20px;
+  width: 100%;
+  max-width: 425px;
+  /* min-width: 300px; */
+  height: 1px;
+  /* position: relative; */
+  bottom: 10px;
+  left: 10px;
+`;
+
 const Button = styled.button`
   flex-shrink: 1;
   width: 150px;
@@ -67,9 +98,23 @@ const Button = styled.button`
   margin: 1em 0px;
   border: 0px;
   border-radius: 30px;
+
+  &:hover {
+    transition: background-color 0.2s ease-in-out;
+    color: #1deff4;
+
+    background: url(${swirlBackground});
+    background-size: 46px;
+    -webkit-text-fill-color: transparent;
+    -webkit-background-clip: text;
+    background-clip: text;
+
+    /* background-color: #f89143; */
+  }
 `;
 
 const LeftWrapper = styled.section`
+  margin-right: 15px;
   display: flex;
   flex-direction: column;
   align-items: flex-end;
@@ -77,6 +122,7 @@ const LeftWrapper = styled.section`
 `;
 
 const RightWrapper = styled.section`
+  margin-left: 15px;
   display: flex;
   flex-direction: column;
   align-items: flex-start;
@@ -84,46 +130,118 @@ const RightWrapper = styled.section`
 `;
 
 export default function NavBar() {
-  return (
-    <Wrapper>
-      <LeftWrapper>
-        <LineTop src={line2} alt="line" />
+  const [windowWidth, setwindowWidth] = useState(window.innerWidth);
+  const [navExpanded, setnavExpanded] = useState(false);
 
-        <LeftContainer>
+  const mobileNavWidth = 800;
+
+  const toggleNavDropdown = () => {
+    const newNavPosition = navExpanded ? false : true;
+    setnavExpanded(newNavPosition);
+  };
+
+  const handleResize = () => {
+    setwindowWidth(window.innerWidth);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  const renderNavDropdown = () => {
+    if (navExpanded)
+      return (
+        <NavDrawer>
           <Button currentPage={false}>
             <Link to="/portfolio">Portfolio</Link>
           </Button>
           <Button currentPage={false}>
             <Link to="/experience">Experience</Link>
           </Button>
-          <Button currentPage={false}>
-            <Link to="/recommendations">Recommendations</Link>
-          </Button>
-        </LeftContainer>
-        <LineBottom src={line} alt="line" />
-      </LeftWrapper>
 
-      <CenterContainer>
-        <Link to="/">
-          <ProfileImage src={profile} alt="Isaac Image" />
-        </Link>
-      </CenterContainer>
-
-      <RightWrapper>
-        <LineTop src={line2} alt="line" />
-        <RightContainer>
-          <Button currentPage={false}>
-            <Link to="/education">Education</Link>
-          </Button>
           <Button currentPage={false}>
             <Link to="/skills">Skills</Link>
           </Button>
           <Button currentPage={false}>
+            <Link to="/education">Education</Link>
+          </Button>
+          <Button currentPage={false}>
+            <Link to="/recommendations">Recommendations</Link>
+          </Button>
+          <Button currentPage={false}>
             <Link to="/hobbies">Hobbies</Link>
           </Button>
-        </RightContainer>
-        <LineBottom src={line} alt="line" />
-      </RightWrapper>
-    </Wrapper>
-  );
+          <LineBottomNav src={line} alt="line" />
+        </NavDrawer>
+      );
+  };
+
+  if (windowWidth < mobileNavWidth)
+    return (
+      <>
+        <Wrapper>
+          <CenterContainer>
+            <Link to="/">
+              <ProfileImage src={profile} alt="Isaac Image" />
+            </Link>
+          </CenterContainer>
+        </Wrapper>
+        <BurgerContainer>
+          <Burger
+            onClick={toggleNavDropdown}
+            src={burgerSolid}
+            alt="Burger Icon"
+          />
+        </BurgerContainer>
+        {renderNavDropdown()}
+      </>
+    );
+
+  if (windowWidth > mobileNavWidth)
+    return (
+      <Wrapper>
+        <LeftWrapper>
+          <LineTop src={line2} alt="line" />
+
+          <LeftContainer>
+            <Button currentPage={false}>
+              <Link to="/portfolio">Portfolio</Link>
+            </Button>
+            <Button currentPage={false}>
+              <Link to="/experience">Experience</Link>
+            </Button>
+
+            <Button currentPage={false}>
+              <Link to="/skills">Skills</Link>
+            </Button>
+          </LeftContainer>
+          <LineBottom src={line} alt="line" />
+        </LeftWrapper>
+
+        <CenterContainer>
+          <Link to="/">
+            <ProfileImage src={profile} alt="Isaac Image" />
+          </Link>
+        </CenterContainer>
+
+        <RightWrapper>
+          <LineTop src={line2} alt="line" />
+          <RightContainer>
+            <Button currentPage={false}>
+              <Link to="/education">Education</Link>
+            </Button>
+            <Button currentPage={false}>
+              <Link to="/recommendations">Recommendations</Link>
+            </Button>
+            <Button currentPage={false}>
+              <Link to="/hobbies">Hobbies</Link>
+            </Button>
+          </RightContainer>
+          <LineBottom src={line} alt="line" />
+        </RightWrapper>
+      </Wrapper>
+    );
 }
